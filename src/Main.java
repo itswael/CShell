eac5614dde97f190dd728929f3463731af63cdb9
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2024-12-20 22:39:55 -0500

    added functionality to take the input from user and display invalid command

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("$ "); //prompt to display by default at the start of each input line
         Scanner scanner = new Scanner(System.in); // helper to take the input
+
+        // 1. Take input from the user via command Line
+        String input = scanner.nextLine();
+        // 2. Parse the command from the input text
+        // 3. Call the respective functionality to execute the command
+            // a. fetch the required parameters from the input text
+            // b. execute the command
+            // c. return the output of the command to main function
+        // 4. Display the output received from the subFunction on the console
+        // If the command is not supported Display Invalid Command
+        System.out.println("Invalid Command");
     }
 }
