f90726314ef20ec774bab5455185dee0c8d8ba6d
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2024-12-22 12:34:05 -0500

    added type command to check if the command is supported, added support to execute a file and capture the output

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in); // helper to take the input
+        ArrayList<String> commands = new ArrayList<>(Arrays.asList("exit", "echo", "type"));
         while(true) {
             // 1. Take input from the user via command Line
             System.out.print("$ "); //prompt to display by default at the start of each input line
@@ -17,12 +23,43 @@ public class Main {
             // c. return the output of the command to main function
             // 4. Display the output received from the subFunction on the console
             // If the command is not supported Display Invalid Command
-            if (Command.equals("echo"))
+            if (Command.equals("echo")) {
                 System.out.println(input.substring(5));
-            else if (Command.equals("exit"))
+            }else if (Command.equals("exit")) {
                 System.exit(0);
-            else
-                System.out.println("Invalid Command");
+            }else if(Command.equals("type")){
+                String param = input.split(" ")[1];
+                if(commands.contains(param)){
+                    System.out.println(param+" is CShell builtin");
+                }else{
+                    String path = getPath(param);
+                    if (path != null) {
+                        System.out.println(param + " is " + path);
+                    } else {
+                        System.out.println(param + ": not found");
+                    }
+                }
+            }else {
+                String command = input.split(" ")[0];
+                String path = getPath(command);
+                if (path == null) {
+                    System.out.printf("%s: command not found%n", command);
+                } else {
+                    String fullPath = path + input.substring(command.length());
+                    Process p = Runtime.getRuntime().exec(fullPath.split(" "));
+                    p.getInputStream().transferTo(System.out);
+                }
+            }
         }
+    }
+
+    private static String getPath(String parameter) {
+        for (String path : System.getenv("PATH").split(":")) {
+            Path fullPath = Path.of(path, parameter);
+            if (Files.isRegularFile(fullPath)) {
+                return fullPath.toString();
+            }
+        }
+        return null;
     }
 }
