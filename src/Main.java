f50b0dd74c18dcb5b1369991b3fe3b759a77cef3
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2024-12-22 12:20:30 -0500

    added continuous prompt

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,25 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.print("$ "); //prompt to display by default at the start of each input line
         Scanner scanner = new Scanner(System.in); // helper to take the input
-
-        // 1. Take input from the user via command Line
-        String input = scanner.nextLine();
-        // 2. Parse the command from the input text
-        String Command = input.split(" ")[0];
-        // 3. Call the respective functionality to execute the command
+        while(true) {
+            // 1. Take input from the user via command Line
+            System.out.print("$ "); //prompt to display by default at the start of each input line
+            String input = scanner.nextLine();
+            // 2. Parse the command from the input text
+            String Command = input.split(" ")[0];
+            // 3. Call the respective functionality to execute the command
             // a. fetch the required parameters from the input text
             // b. execute the command
             // c. return the output of the command to main function
-        // 4. Display the output received from the subFunction on the console
-        // If the command is not supported Display Invalid Command
-        if(Command.equals("echo"))
-            System.out.println(input.substring(5));
-        else if(Command.equals("exit"))
-            System.exit(0);
-        else
-            System.out.println("Invalid Command");
+            // 4. Display the output received from the subFunction on the console
+            // If the command is not supported Display Invalid Command
+            if (Command.equals("echo"))
+                System.out.println(input.substring(5));
+            else if (Command.equals("exit"))
+                System.exit(0);
+            else
+                System.out.println("Invalid Command");
+        }
     }
 }
